927ea9f20dfcde25fa9f738da4dac53974d4b915
AlishBro <alishgosai@gmail.com>
2024-11-26 12:54:18 +1100

    sdljf

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Home"
+        initialRouteName="ScanLogin"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="ScanLogin" component={ScanPage} />
