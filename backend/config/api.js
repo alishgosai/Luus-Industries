b882d3905fe423a8a52fed58e161fe4b278777c1
Magar333 <12202333@students.koi.edu.au>
2025-01-16 09:37:38 +1100

    as added

--- backend/config/api.js
+++ backend/config/api.js
@@ -1,5 +1,5 @@
 
-const API_URL = 'http://172.20.10.3:3000'; // Make sure this is your correct server IP and 
+const API_URL = 'http://192.168.0.23:3000'; // Make sure this is your correct server IP and 
 
 
 
